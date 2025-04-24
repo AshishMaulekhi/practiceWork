0324b6a1760687bfe8967519aad4d1875de9fa4f
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-24 15:04:13 +0530

    Info file

--- ashish/my-port/src/About/Info.tsx
+++ ashish/my-port/src/About/Info.tsx
@@ -5,5 +5,65 @@ const Info = {
    "stack":["Full Stack Web Developer","Competitive Programmer","Freelancer","Open Source Contributer"]
    
 }
+const ProjectInfo=[
+   {
+   title:"Job Portal",
+   desc:"Job Portal is modern, fully responsive job search platform  built using typeScript, Tailwind, MantineUI, SpringBoot, MongoDb offering a seamless connection between job seekers with potential employers. It allows users to create profiles, upload resumes, and search for job listings based on various criteria. Employers can post job openings, review applications, email alerts, application tracking and filter candidates. Job portals make finding and applying for jobs as easy as using the internet. ",
+   image:"jobPortal.png",
+   live:true,
+   technologies:["TypeScript","TailwindCSS","Redux","MantineUI","SpringBoot","MongDb"],
+   link:"https://github.com/AshishMaulekhi/",
+   github:"https://github.com/AshishMaulekhi/Project-works/",
+   },
+   {
+      title:"Airbnbs Hotel Management System",
+      desc:"An Airbnb-style hotel management system helps guests book rooms online easily. It lets hosts list properties, set prices, and manage availability. Guests can search, compare, and reserve rooms based on location and needs. The system includes features like payments, reviews, and booking history. It makes hotel and property management simple, digital, and user-friendly",
+      image:"jobPortal.png",
+      live:false,
+      technologies:["TypeScript","TailwindCSS","Redux","MantineUI","SpringBoot","MongDb"],
+      link:"https://github.com/AshishMaulekhi/",
+      github:"https://github.com/AshishMaulekhi/Project-works/",
+   },
+   {
+      title:"linkedIn MicroService System",
+      desc:" A LinkedIn Microservice System breaks the platform into small, independent services. Each service handles a specific feature like profiles, messaging, jobs, or connections. They communicate using APIs to keep the system scalable and maintainable. This architecture improves performance, fault isolation, and development speed. It makes managing a large social network platform efficient and flexible.",
+      image:"jobPortal.png",
+      live:true,
+      technologies:["TypeScript","TailwindCSS","Redux","MantineUI","SpringBoot","MongDb"],
+      link:"https://github.com/AshishMaulekhi/",
+      github:"https://github.com/AshishMaulekhi/Project-works/",
+   },
 
-export  {Info}
+
+]
+const SkillInfo = [
+   {
+       title: "Frontend",
+       skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Redux", "Tailwind CSS", "Material UI"
+       ]
+   },
+   {
+       title: "Backend",
+       skills: ["Springboot", "MySQL", "MongoDB", "Firebase", "PostgresSQL"]
+   },
+   {
+       title: "Languages",
+       skills: ["C",  "Java", "JavaScript", "TypeScript"]
+   },
+   {
+       title: "Tools",
+       skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass", "Spring Tool Suite"]
+   }
+]
+
+const ExperienceInfo = [
+   {
+       role: "Frontend Devloper",
+       company: "MangoOranges",
+       date: "Jan 2024 - March 2024 ",
+       desc: "I led Frontend development with  React and TypeScript, creating responsive UI and interfaces. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
+       skills: [ "React JS", "TypeScript", "Mantine UI","shadcn" ]
+   }
+]
+
+export  {Info,ProjectInfo,SkillInfo,ExperienceInfo}
