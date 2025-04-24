9893a8174c78282ea738e6c4785441f884c8f7f9
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-24 15:05:40 +0530

    Root layout file contain all sub file of home page :

--- ashish/my-port/src/MainLayout/RootLayouts.tsx
+++ ashish/my-port/src/MainLayout/RootLayouts.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Mail from '../Components/Mail'
 import Social from '../Components/Social'
 import Abouts from '../About/Abouts'
+import MainProject from '../Projects/MainProject'
+import MainSkills from '../Skill/MainSkills'
+import MainExperience from '../Experience/MainExperience'
 
 const RootLayouts = () => {
   return (
@@ -9,6 +12,9 @@ const RootLayouts = () => {
       <Abouts/>
       <Mail/>
       <Social/>
+      <MainProject/>
+      <MainSkills/>
+      <MainExperience/>
 
     </div>
   )
