677819effd5e1a10b0dce92dc33d87cd851c7505
AshishMaulekhi <ashishmaulekhi12@gmail.com>
2025-04-26 21:15:03 +0530

    cdn file commited

--- ashish/my-port/src/App.tsx
+++ ashish/my-port/src/App.tsx
@@ -9,10 +9,12 @@ import 'aos/dist/aos.css';
 
 import { useEffect } from "react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
 
 function App() {
   useEffect(()=>{
